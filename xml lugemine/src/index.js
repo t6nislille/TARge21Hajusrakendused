bcee05ecbf019ff356724271c63c73124c2b0a0e
Tõnis <tonislille@gmail.com>
2023-01-12 15:57:04 +0200

    XML lugemine

--- xml lugemine/src/index.js
+++ xml lugemine/src/index.js
@@ -7,16 +7,23 @@ const getXml = function(fileName){
     return xmlhttp.responseXML
 }
 
+const getPlatforms = function(htmlCollection ){
+    return[...htmlCollection].map(element => element.childNodes[0].nodeValue).join("/")
+}
+
 const generateTable = function(XMLContent) {
-    let tableRows = "<tr><th>Title</th><th>Price</th></tr>"
+    let tableRows = "<tr><th>Title</th><th>Price</th><th>Platforms</th></tr>"
     const gameElements = XMLContent.getElementsByTagName("game")
     for (let i = 0; i < gameElements.length; i++) {
         const game = gameElements[i];
-        tableRows += "<tr><td>" +
+        tableRows += 
+            "<tr><td>" +
                 game.getElementsByTagName("title")[0].childNodes[0].nodeValue + 
             "</td><td>" +
                 game.getElementsByTagName("price")[0].childNodes[0].nodeValue +
-            "</td></tr>"
+            "</td><td>" +
+                getPlatforms (game.getElementsByTagName("platform")) +
+            "</td></tr>";
     }
     document.getElementById("xmlTable").innerHTML = tableRows
 }
